0a4b79c9c21ecc2fd0d0adb6cf5a3df8936fb616
Hammi <alex.hammans@gmail.com>
2023-01-29 09:07:28 +0100

    Created new SCPI_Parser Folder

---
+++ SCPI_Parser/SCPI.h
@@ -0,0 +1,10 @@
+/**
+ * @file SCPI.h
+ * @author Alexander Hammans (a.hammans@kostal.com)
+ * @brief This is just a wrapper for the whole SCPI_Parser library.
+ * @version 0.1
+ * @date 29.01.2023
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
